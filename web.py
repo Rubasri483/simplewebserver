3c80d200d6234d0309a0a2a3fd71ef59d1f4c432
Rubasri483 <rshankar83430@gmail.com>
2025-04-08 22:21:06 +0530

    Success

---
+++ web.py
@@ -0,0 +1,45 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+content = """
+<html>
+<head>
+    <h1 style="font-family: Arial, sans-serif; color: brown; text-align: center;"><b>LIST OF PROTOCOLS</b></h1>
+    <h1 style="font-family: Arial, sans-serif; color: black; text-align: center; font-size: 18px;">
+        NAME: RUBASRI.R<br> 
+        REF NO: 212224240139
+    </h1>
+    
+    <title>TCP/IP Protocol Suite</title>
+</head>
+<body>
+    <h1 style="font-family: Arial, sans-serif; font-style: italic; text-decoration: underline;text-align: center;">
+        TCP/IP Protocol Suite
+    </h1>
+    
+    <div style="text-align: center;">
+        <ul style="list-style-type: none; padding-left: 0; font-size: 20px;font-family: 'Open Sans', sans-serif;">
+            <li>&#8594; HTTP: Hypertext Transfer Protocol</li>
+            <li>&#8594; FTP: File Transfer Protocol</li>
+            <li>&#8594; SMTP: Simple Mail Transfer Protocol</li>
+            <li>&#8594; DNS: Domain Name System</li>
+            <li>&#8594; Telnet: Telecommunications Network</li>
+            <li>&#8594; SNMP: Simple Network Management Protocol</li>
+        </ul>
+    </div>
+    
+    
+    
+    
+</body>
+</html>
+"""
+class myhandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("request received")
+        self.send_response(200)
+        self.send_header('content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
+server_address = ('',8000)
+httpd = HTTPServer(server_address,myhandler)
+print("my webserver is running...")
+httpd.serve_forever()
